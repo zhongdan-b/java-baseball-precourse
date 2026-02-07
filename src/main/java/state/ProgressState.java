0e572744ab04805cab2fd9cc1a087951b752d0c5
zhongdan-b <remi.ni@kakaocorp.com>
2026-02-07 20:33:06 +0900

    feat: progressState 입력 처리와 결과 출력 흐름 구현

--- src/main/java/state/ProgressState.java
+++ src/main/java/state/ProgressState.java
@@ -2,15 +2,38 @@ package state;
 
 import model.BaseballNumber;
 import model.Game;
+import model.GameResult;
+import view.Inputview;
+import view.OutputView;
 
 public class ProgressState implements GameState{
+    private final BaseballNumber answer;
+    private final Inputview inputview;
+    private final OutputView outputView;
 
     public ProgressState(BaseballNumber baseballNumber){
-
+        this.answer = baseballNumber;
+        this.inputview = new Inputview();
+        this.outputView = new OutputView();
     }
 
     @Override
     public void handle(Game game){
+        outputView.printInputGuide();
+        String input = inputview.readNumber();
+        playOneTurn(input, game);
+    }
 
+    private void playOneTurn(String input, Game game){
+        try {
+            BaseballNumber userNumber = new BaseballNumber(input);
+            GameResult gameResult = answer.compare(userNumber);
+            outputView.printResult(gameResult.getMessage());
+            if(!gameResult.isThreeStrike()) return;
+            outputView.printGameEnd();
+            game.changeState(new GameOverState());
+        } catch (IllegalArgumentException e){
+            outputView.printError(e.getMessage());
+        }
     }
 }
